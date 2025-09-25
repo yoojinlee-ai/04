d6c958076fe308b12e410b53b4dcae607fc8456c
yoojinlee-ai <yoojinlee@sookmyung.ac.kr>
2025-09-25 15:59:34 +0900

    4-4

--- main.c
+++ main.c
@@ -4,16 +4,15 @@
 int main() {
     int year;
 
-    printf("Input year :");
-    scanf("%i", &year);  
-    
-    
-    printf("If the year is %i leap year? %s\n", year,
-        ( (year%4 == 0 && year%100 != 0) || (year%400 == 0) ) ? "Yes" : "No" );
+    printf("Input year: ");
+    scanf("%i", &year);
+
+   
+    printf("Is the year %i leap year? %s\n", year,
+        ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) ? "Yes" : "No");
 
     system("PAUSE");
 
     return 0;
 }
-
 
