50a2d09c7b8c7733560ab77af2328acc20c68aae
yoojinlee-ai <yoojinlee@sookmyung.ac.kr>
2025-09-25 15:42:54 +0900

    04-2

--- main.c
+++ main.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    int x, Y;
-    
-    printf("Input two integers : ");
-    scanf("%i %i", &x, &Y);
+int main() {
+    int x, y;
 
-    printf("+ result : %i\n", x + Y);
-    printf("- result : %i\n", x - Y);
-    printf("* result : %i\n", x * Y);
-    printf("/ result : %i\n", x / Y);   
-    printf("%% result : %i\n", x % Y);
+    printf("input two integers: ");
+    scanf("%i %i", &x, &y);
+
+    
+
+    printf("+ result : %i\n", x+y);
+
     system("PAUSE");
-  
+
     return 0;
 }
 
