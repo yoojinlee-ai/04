0edf88e68049932db25364694159dc4b7b05b98a
yoojinlee-ai <yoojinlee@sookmyung.ac.kr>
2025-09-25 15:38:26 +0900

    4-2

--- main.c
+++ main.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    int x, y, z, m;
-    int a, b, c;
+    int x, y;
 
-    x = 2;
-    z = 1;
-    a = 3;
-    b = 4;
-    c = 5;
+    printf("input two integers: ");
+    scanf("%i %i", &x, &y);
 
-    // Calculation of y = a*x^2 + b*x + c
-    // Use proper operator for power: x*x for x squared
-    y = a * x * x + b * x + c;
+    
 
-    // Calculation of average m = (x + y + z) / 3
-    m = (x + y + z) / 3;
+    printf("+ result : %i\n", x+y);
 
-    printf("y=%d, m=%d\n", y, m);
+    system("PAUSE");
 
     return 0;
 }
